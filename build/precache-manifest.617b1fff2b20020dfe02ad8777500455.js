9fd5a2493492cd9cb4778067c9da25165f620cd5
Nam Doan <nam.quang.doan@gmail.com>
2020-10-14 10:14:40 -0400

    Frontend

---
+++ build/precache-manifest.617b1fff2b20020dfe02ad8777500455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfe9853d4aab7e751bfdec53f62b568",
+    "url": "/index.html"
+  },
+  {
+    "revision": "004977a2608491b3b5f9",
+    "url": "/static/css/main.97192864.chunk.css"
+  },
+  {
+    "revision": "d2c6dcf6a94361e5a983",
+    "url": "/static/js/2.c2ba68e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2ba68e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004977a2608491b3b5f9",
+    "url": "/static/js/main.fec60b07.chunk.js"
+  },
+  {
+    "revision": "23bb1571825c4fcbd490",
+    "url": "/static/js/runtime-main.bd624b67.js"
+  }
+]);
